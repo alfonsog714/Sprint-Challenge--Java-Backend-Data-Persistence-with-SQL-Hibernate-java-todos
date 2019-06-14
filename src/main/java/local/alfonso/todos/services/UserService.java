9ead342acf73f5dfbf7d9295bc7cd7bac3c48401
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-14 08:40:14 -0700

    Added services

---
+++ src/main/java/local/alfonso/todos/services/UserService.java
@@ -0,0 +1,20 @@
+package local.alfonso.todos.services;
+
+
+import local.alfonso.todos.models.User;
+
+import java.util.List;
+
+public interface UserService
+{
+
+    List<User> findAll();
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
+}

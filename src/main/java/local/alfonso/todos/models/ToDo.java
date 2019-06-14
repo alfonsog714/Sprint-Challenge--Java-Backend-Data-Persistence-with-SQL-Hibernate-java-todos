1cc0facd78d831b1b14930f75f602e18583e7bff
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-14 08:50:36 -0700

    Added ToDo class

---
+++ src/main/java/local/alfonso/todos/models/ToDo.java
@@ -0,0 +1,24 @@
+package local.alfonso.todos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+public class ToDo extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    @JsonIgnoreProperties("todo")
+    @Column(nullable = false)
+    private User userid;
+}
